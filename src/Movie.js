bd91548c6afa73e5707546e830d009de012b0ede
Luna <boreum4151@gmail.com>
2019-03-09 17:51:03 +0900

    change class component into functional component in Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -1,34 +1,27 @@
-import React, {Component}from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
+function Movie({title, poster}){
+    return(
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+    </div>
+    )
+} 
 
-    static propTypes ={
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-    //component func은 항상 show something 그래서 render를 꼭 작성해야해
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
 }
-class MoviePoster extends Component {
 
-    static propTypes={
-        poster: PropTypes.string.isRequired
-    }
+function MoviePoster({poster}){
+    return <img src={poster} alt="Movie poster"/>
+}
 
-    render() {
-        return (
-            <img src={this.props.poster} alt="Movie poster"/>
-        )
-    }
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
