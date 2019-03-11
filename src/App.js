29c724370c43c878c543589980138949f79a5fbd
Luna <boreum4151@gmail.com>
2019-03-11 18:55:53 +0900

    get movie data with api and use linesellipsis

--- src/App.js
+++ src/App.js
@@ -11,18 +11,40 @@ class App extends Component {
   state={  }
 
   componentDidMount(){
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-    .then(response => response.json())
-    //밑에 then은 response를 json으로 바꾸고 그러고 then
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._getMovies();
   }
 
   _renderMovies = ()=>{
-    const movies = this.state.movies.map((movie, index)=>{
-      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    const movies = this.state.movies.map(movie=>{
+      return <Movie 
+              title={movie.title_english} 
+              poster={movie.medium_cover_image} 
+              key={movie.id}
+              genres = {movie.genres}
+              synopsis = {movie.synopsis}
+              />
     })
     return movies;
+  }
+
+  // function들을 세분화시켜줘
+  //asynchronous : 이전 작업이 끝나야 그 다음작업이 시작하는것이 아니야 순서와 상관없이 진행됨
+   _getMovies = async() =>{
+    const movies = await this._callApi()
+    //await는 기다리라는 의미 this._callApi가 끝날때까지 기다려 라는 의미 이게 성공적아든 아니든 
+    //call api 작업이 완료되고 return 하기를 await하다 
+    //그리고 밑에 setState는 위에 callApi가 끝나기 전까지 movies에 return 되기 전까지 작동하지 않아
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    //밑에 then은 response를 json으로 바꾸고 그러고 then
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render() {
--- src/Movie.js
+++ src/Movie.js
@@ -1,27 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import LinesEllipsis from 'react-lines-ellipsis';
 
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis}){
     return(
         <div>
-            <MoviePoster poster={poster}/>
-            <h1>{title}</h1>
-    </div>
-    )
+            <div className="Movie__Col">
+                <MoviePoster poster={poster} alt={title}/>
+            </div>
+            <div className="Movie__Col">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+                </div>
+                <p className="Movie__Syno">
+                    <LinesEllipsis
+                        text={synopsis}
+                        maxLine='3'
+                        ellipsis='...'
+                        trimRight
+                        basedOn='letters'
+                    />
+                </p>
+            </div>
+        </div>
+    );
 } 
 
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
-function MoviePoster({poster}){
-    return <img src={poster} alt="Movie poster"/>
+function MoviePoster({poster, alt}){
+    return <img src={poster} alt={alt} title={alt} className="Movie__Poster"/>
 }
 
 MoviePoster.propTypes = {
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+function MovieGenre({genre}){
+    return <span className="Movie__Genre">{genre}</span>
+}
+
+MovieGenre.propTypes={
+    genre:PropTypes.string.isRequired
 }
 
 export default Movie;
